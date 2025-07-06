bdff00b02710cc9f4ccfec228da27c1aa8d3e6fc
linhvv <linhvv@devup.edu.vn>
2025-07-06 10:55:27 +0700

    sua lai cau truc project va update

--- crawl_data/crawl.py
+++ crawl_data/crawl.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import time
+import random
 
-def  get_token():
+#ham get token access de crawl du lieu befood
+def get_token():
     url = "https://gw.be.com.vn/api/v1/be-delivery-gateway/api/v1/user/guest"
 
     payload = json.dumps({
@@ -32,6 +35,7 @@ def  get_token():
     "latitude": None,
     "longitude": None
     })
+
     headers = {
     'accept': '*/*',
     'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
@@ -55,11 +59,13 @@ def  get_token():
     return json.loads(response.text)["access_token"]
 
 
+#Ham lay thong tin cac nha hang
 def get_restaurent(page, latitude, longitude):
 
     access_token = get_token()
 
     url = "https://gw.be.com.vn/api/v1/be-marketplace/web/collection/items/restaurants"
+
     payload = json.dumps({
     "collection_id": "231",
     "page": page,
@@ -90,6 +96,7 @@ def get_restaurent(page, latitude, longitude):
     "latitude": latitude,
     "longitude": longitude
     })
+
     headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0',
     'Accept': '*/*',
@@ -110,5 +117,88 @@ def get_restaurent(page, latitude, longitude):
 
     return requests.request("POST", url, data=payload, headers= headers).text
 
-data_response = json.loads(get_restaurent(page = 1,latitude=10.826233309253325,longitude=106.62746414326836))
-print(data_response)
+#Ham lay ra thong tin chi tiet cua nha hang
+def get_detail_restaurants(restaurant_id):
+    access_token = get_token()
+
+    url = "https://gw.be.com.vn/api/v1/be-marketplace/web/restaurant/detail"
+
+    payload = json.dumps({
+    "restaurant_id": restaurant_id,
+    "locale": "vi",
+    "app_version": "11280",
+    "version": "1.1.280",
+    "device_type": 3,
+    "operator_token": "0b28e008bc323838f5ec84f718ef11e6",
+    "customer_package_name": "xyz.be.food",
+    "device_token": "43be67cda697604c0781023d055ef5d7",
+    "ad_id": "",
+    "screen_width": 360,
+    "screen_height": 640,
+    "client_info": {
+        "locale": "vi",
+        "app_version": "11280",
+        "version": "1.1.280",
+        "device_type": 3,
+        "operator_token": "0b28e008bc323838f5ec84f718ef11e6",
+        "customer_package_name": "xyz.be.food",
+        "device_token": "43be67cda697604c0781023d055ef5d7",
+        "ad_id": "",
+        "screen_width": 360,
+        "screen_height": 640
+    }
+    })
+
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Content-Type': 'application/json',
+    'Referer': 'https://food.be.com.vn/',
+    'app_version': '11280',
+    'version': '1.1.280',
+    'Origin': 'https://food.be.com.vn',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'Authorization': f'Bearer {access_token}',
+    'Connection': 'keep-alive',
+    'TE': 'trailers'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return json.loads(response.text)
+
+
+def combie_data(restaurant):
+    restaurant_id = restaurant["restaurant_id"]
+    data_detail_restaurant = get_detail_restaurants(restaurant_id)["data"]["categories"]
+    restaurant["data_detail_restaurant"] = data_detail_restaurant
+
+    return restaurant
+    
+    
+
+def insert_to_mongo():
+    for i in range(0,100):
+        data_response = json.loads(get_restaurent(page = i,latitude=10.826233309253325,longitude=106.62746414326836))
+        data = data_response["data"]
+
+        if data:
+            for res in data:
+                full_data_restaurant = combie_data(res)
+                time.sleep(random.randint(1,2))
+        else:
+            break
+
+        time.sleep(random.randint(1,6))
+
+
+                
+
+                
+
+            
+
